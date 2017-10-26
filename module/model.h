73b39e26298d6214001596a9f29da655efca8d65
Pierre Carl Michaud <michaudp@mbp.local>
2017-10-26 07:51:37 -0400

    updates

--- module/model.h
+++
@@ -1,5 +0,0 @@
-integer g_nsim 			! number of simulated individuals 
-integer g_npar			! number of parameters
-integer g_agemin		! start age
-integer g_agemax		! maximum age
-integer g_nages			! number of ages 
